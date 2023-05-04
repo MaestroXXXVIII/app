37fd7199a0eec5ed2558f12cd7ec6d99c279f8fe
MRROBOT <urmanov.2000@mail.ru>
2023-05-04 19:47:16 +0800

    Added the ability to recommend posts by email

--- app/app/settings.py
+++ app/app/settings.py
@@ -124,6 +124,15 @@ STATIC_URL = '/static/'
 STATIC_DIR = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
+# SMTP SETTINGS
+
+# EMAIL_HOST = 'smtp.gmail.com'
+# EMAIL_HOST_USER = 'my_account@gmail.com'
+# EMAIL_HOST_PASSWORD = ''
+# EMAIL_PORT = 587
+# EMAIL_USE_TLS = True
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
