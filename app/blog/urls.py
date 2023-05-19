9bde27dffab7cdf4355593f80ffbe994993dc547
MRROBOT <urmanov.2000@mail.ru>
2023-05-19 17:29:22 +0800

    Adding full-text search

--- app/blog/urls.py
+++ app/blog/urls.py
@@ -13,4 +13,5 @@ urlpatterns = [
     path('<int:post_id>/share/', views.post_share, name='post_share'),
     path("comment/<int:pk>/", views.AddComment.as_view(), name="add_comment"),
     path('feed/', LatestPostsFeed(), name='post_feed'),
+    path('search/', views.AddSearch.as_view(), name='post_search'),
 ]
