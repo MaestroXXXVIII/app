73ea975fc98d8f6bd5585de00ca7adad76d7dd3b
MRROBOT <urmanov.2000@mail.ru>
2023-05-25 15:50:09 +0800

    Changed some functions to classes

--- app/blog/views.py
+++ app/blog/views.py
@@ -1,13 +1,12 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
 from .models import Post
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .forms import EmailPostForm, CommentForm, SearchForm
 from django.db.models import Count
 from django.core.mail import send_mail
 from django.contrib.postgres.search import SearchVector, SearchQuery,\
                                            SearchRank
-
 
 
 class PostListView(ListView):
@@ -28,29 +27,23 @@ class TagIndexView(ListView):
         return Post.objects.filter(tags__slug=self.kwargs.get('tag_slug') )
 
 
-
-def post_detail(request, year, month, day, post):
-    post = get_object_or_404(Post,
-                             status=Post.Status.PUBLISHED, 
-                             slug=post,
-                             publish__year=year,
-                             publish__month=month,
-                             publish__day=day)
-    # list active comments
-    comments = post.comments.filter(active=True)
-    form = CommentForm()
+class PostDetail(DetailView):
     
-    post_tags_ids = post.tags.values_list('id', flat=True)
-    similar_posts = Post.published.filter(tags__in=post_tags_ids)\
-                                  .exclude(id=post.id)
-    similar_posts = similar_posts.annotate(same_tags=Count('tags'))\
-                                 .order_by('-same_tags', '-publish')[:4]
-    return render(request, 'blog/post/detail.html', {'post': post,
-                                                     'comments': comments,
-                                                     'form': form,
-                                                     'similar_posts': similar_posts})
+    slug_field = 'slug'
+    queryset = Post.published.all()
+    template_name = 'blog/post/detail.html'
 
-
+    def get_context_data(self, *args, **kwargs):
+        context = super().get_context_data(*args, **kwargs)
+        post = Post.objects.values_list('id', flat=True)
+        post_tags_ids = Post.tags.values_list('id', flat=True)
+        similar_posts = Post.published.filter(tags__in=post_tags_ids)\
+                                      .exclude(id=post[:1])
+        context["similar_posts"] = similar_posts.annotate(same_tags=Count('tags'))\
+                                  .order_by('-same_tags', '-publish')[:4]
+        context["form"] = CommentForm()
+        return context
+    
 
 def post_share(request, post_id):
     post = get_object_or_404(Post, id=post_id, status=Post.Status.PUBLISHED)
